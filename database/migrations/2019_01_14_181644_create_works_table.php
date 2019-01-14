64fa909bd8778c0b530ae7bbe86d70ea3672b86c
Zhenyah <Zhenyauk@gmail.com>
2019-01-14 20:39:50 +0200

    Users Cats Works tables

---
+++ database/migrations/2019_01_14_181644_create_works_table.php
@@ -0,0 +1,42 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateWorksTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('works', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title')->nullable();
+            $table->integer('category_id')->unsigned()->nullable()->default(null);
+            $table->foreign('category_id')->references('id')->on('categories')->onUpdate('cascade')->onDelete('set null');
+            $table->integer('company_id')->nullable();
+            $table->text('description')->nullable();
+            $table->text('apply')->nullable();
+            $table->integer('active')->default(1);
+            $table->integer('important')->default(0);
+            $table->integer('highlight')->default(0);
+            $table->dateTime('from')->nullable();
+            $table->integer('job_posted')->default(0);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('works');
+    }
+}
---
+++ database/migrations/2019_01_14_182310_create_companies_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateCompaniesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('companies', function (Blueprint $table) {
+            $table->increments('id');
+            $table->string('title');
+            $table->text('description')->nullable();
+            $table->string('logo')->nullable();
+            $table->string('head')->nullable();
+            $table->string('location')->nullable();
+            $table->string('site')->nullable();
+            $table->integer('active')->default(1);
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('companies');
+    }
+}
